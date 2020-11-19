fcfa1ec1566ab57c35515e1ada8373782864f7a3
yorgomoubayed <41142299+yorgomoubayed@users.noreply.github.com>
2020-11-19 10:11:14 +0100

    Update importdata.py

--- importdata.py
+++ importdata.py
@@ -17,8 +17,6 @@ with open(CSV_PATH, newline ="") as csvfile:
 
         if(row[9] == 'X'):
             row[9] = row[9].replace('X', '55') # conversion du chromosome X par 55
-        if(row[15] == 'NR'):
-            row[15] = row[15].replace('NR', '-1') # Conversion de NR (fréquence non renseigné) par rien (1)
         if(row[15] == ''):
             row[15] = row[15].replace('' , '-9999') # -9999 for empty data
         if(row[13] == ''):
@@ -31,12 +29,6 @@ with open(CSV_PATH, newline ="") as csvfile:
             row[9] = row[9].replace('', '-9999') # -9999 for empty data
         if(row[10] == ''):
             row[10] = row[10].replace('', '-9999') # -9999 for empty data
-        Database.objects.create(pumedid= int(row[0]), first_author= row[1], date= row[2], journal = row[3], link= row[4], study = row[5], disease= row[6],
-        initial_sample_size= row[7], region= row[8], chr_id= int(row[9]), chr_pos= int(row[10]), strongest_snp_id_risks= row[11], snps = row[12], snp_id_current= int(row[13]) , context= row[14], risk_allele_frequency= float(row[15]),
-        p_value = float(row[16]),  p_valueLog = float(row[17]) )
-        # print(row)
-        # accumulateur_data += [row[9]]
-#     print(f"{str(count)} succès d'insertion")
-        # print(row[0])
-# print(len(accumulateur_data))
-# print(row[0])
+        Database.objects.create(x= int(row[0]), y= row[1], z= row[2], x= row[3], y=row[4], z=row[5], x= row[6],
+        y= row[7], z= row[8], x= int(row[9]), y= int(row[10]), z= row[11], x = row[12], y= int(row[13]) , z= row[14], x= float(row[15]),
+        y=float(row[16]),  z=float(row[17]) )
